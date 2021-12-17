2327ac2f5f3cecbc91cbb8665d98801a223ae644
Axizeaus <axizeaus@gmail.com>
2021-12-17 23:04:13 +0630

    Add comments and modify run_game()

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,22 +3,34 @@ import sys
 import pygame
 
 
+def run_game():
+    """Running and quitting the game"""
+    while True:
+        # checking the input ( keyboard and mouse )
+        for event in pygame.event.get():
+            # if quit then quit
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        # making the recent screen visible
+        pygame.display.flip()
+
+
 class AlienInvasion(object):
+    """Main code to control game flow."""
+
     def __init__(self):
+
+        """Initializing the game."""
         pygame.init()
 
+        # making a screen with 1200, 800
         self.screen = pygame.display.set_mode((1200, 800))
+        # making that little line on the game window
         pygame.display.set_caption("Alien Invasion version 1.0")
-
-    def run_game(self):
-        while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-
-            pygame.display.flip()
 
 
 if __name__ == '__main__':
+    # make an instance of the class AlienInvasion() and running it
     ai = AlienInvasion()
-    ai.run_game()
+    run_game()
