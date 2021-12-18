5f69de66d6a3f6eb02055f5a009fde4d7c7cd5e2
Axizeaus <axizeaus@gmail.com>
2021-12-19 04:05:11 +0630

    add settings and modify respectively

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,36 +1,38 @@
 import sys
 
 import pygame
-
-
-def run_game():
-    """Running and quitting the game"""
-    while True:
-        # checking the input ( keyboard and mouse )
-        for event in pygame.event.get():
-            # if quit then quit
-            if event.type == pygame.QUIT:
-                sys.exit()
-
-        # making the recent screen visible
-        pygame.display.flip()
+from settings import Settings
 
 
 class AlienInvasion(object):
     """Main code to control game flow."""
 
     def __init__(self):
-
         """Initializing the game."""
         pygame.init()
+        self.settings = Settings()
 
         # making a screen with 1200, 800
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         # making that little line on the game window
         pygame.display.set_caption("Alien Invasion version 1.0")
+
+    def run_game(self):
+        """Running and quitting the game"""
+        while True:
+            # checking the input ( keyboard and mouse )
+            for event in pygame.event.get():
+                # if quit then quit
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            # redraw the screen during each pass of the loop
+            self.screen.fill(self.settings.bg_colour)
+
+            # making the recent screen visible
+            pygame.display.flip()
 
 
 if __name__ == '__main__':
     # make an instance of the class AlienInvasion() and running it
     ai = AlienInvasion()
-    run_game()
+    ai.run_game()
---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings:
+    """Settings for alien invasion"""
+
+    def __init__(self):
+        """initialise game's setting"""
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_colour = (150, 200, 150)
