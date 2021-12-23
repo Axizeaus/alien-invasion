26b0c1871d5b6026510323de233dc8b4a966a98a
Axizeaus <axizeaus@gmail.com>
2021-12-23 23:01:32 +0630

    Added ship.py and ship.bmp, modified alien_invasion code flow, and modified setting.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,7 +1,8 @@
 import sys
-
 import pygame
+
 from settings import Settings
+from ship import Ship
 
 
 class AlienInvasion(object):
@@ -16,20 +17,33 @@ class AlienInvasion(object):
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         # making that little line on the game window
         pygame.display.set_caption("Alien Invasion version 1.0")
+        self.ship = Ship(self)
 
     def run_game(self):
         """Running and quitting the game"""
         while True:
             # checking the input ( keyboard and mouse )
-            for event in pygame.event.get():
-                # if quit then quit
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
             # redraw the screen during each pass of the loop
-            self.screen.fill(self.settings.bg_colour)
+            self._update_screen()
 
-            # making the recent screen visible
-            pygame.display.flip()
+    def _check_events(self):
+        """Respond the keyboard and mouse movement"""
+        for event in pygame.event.get():
+            # if quit then quit
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.type == pygame.K_RIGHT:
+                    #move the ship to the right
+                    self.ship.rect.x += 1000
+
+
+    def _update_screen(self):
+        """Update images on the screen and flip to a new screen"""
+        self.screen.fill(self.settings.bg_colour)
+        self.ship.blitme()
+        pygame.display.flip()
 
 
 if __name__ == '__main__':
