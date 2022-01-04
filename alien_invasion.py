86e43567a6462be82c735124d06de433e011d32e
Axizeaus <axizeaus@gmail.com>
2022-01-04 22:13:07 +0630

    create new fleet once all the aliens are destroyed

--- alien_invasion.py
+++ alien_invasion.py
@@ -99,6 +99,11 @@ class AlienInvasion(object):
         # check for any bullet that has hit aliens
         # if so get rid of the bullet and the alien
         collisions = pygame.sprite.groupcollide(self.bullets,self.aliens,True, True)
+        if not self.aliens:
+            # Destroy existing bullets and create new fleet
+            self.bullets.empty()
+            self._create_fleet()
+
 
     def _create_fleet(self):
         """Create the fleet of aliens"""
